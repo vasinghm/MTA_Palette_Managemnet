3ebe83e6792564c0221e2989fbbb1d9077d23abf
Vaibhav <vasinghm@in.ibm.com>
2020-05-26 14:29:16 +0530

    commit message

---
+++ UI_Palette_Managemnet/webapp/controller/View1.controller.js
@@ -0,0 +1,8 @@
+sap.ui.define(["sap/ui/core/mvc/Controller" 
+], function (Controller) {
+ "use strict";
+ return Controller.extend("UI_Palette_Managemnet.UI_Palette_Managemnet.controller.View1", {
+onInit: function () {
+}
+});
+});
